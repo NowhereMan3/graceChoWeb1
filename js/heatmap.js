0e16b22dc02f141ca10e5a6530d3a2563183192e
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-20 21:43:01 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -7,14 +7,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map)
 
 
-// add geoJSON points layer*
-async function addCelltowersGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const markers = L.geoJson(data)
- markers.addTo(map)
-}
-addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 
 // default map settings
