1afe1887b11ae53c806e60bf0955839e5b5ed912
NowhereMan3 <147972435+NowhereMan3@users.noreply.github.com>
2024-06-20 23:14:32 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -29,3 +29,9 @@ fetch("geojson/tartu_city_celltowers_edu.geojson")
     })
 
 
+geoJson2heat = ((geojson) => {
+        return geojson.features.map(function(feature) {
+        return [parseFloat(feature.geometry.coordinates[1]), 
+                parseFloat(feature.geometry.coordinates[0])];
+        });
+        });
